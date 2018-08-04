56f70f866cffb65301112aa63491dc832b856710
Ravi Malik <ravimalik2364@gmail.com>
2018-08-04 16:52:36 -0400

    Graphs: BFS

---
+++ graph/graph.py
@@ -0,0 +1,60 @@
+class Queue:
+	def __init__(self):
+		self.queue = []
+
+	def put(self, elem):
+		self.queue.append(elem)
+
+	def get(self):
+		val = self.queue[0]
+		del (self.queue[0])
+		return val
+
+	def __len__(self):
+		return len(self.queue)
+
+	def __str__(self):
+		return self.queue.__str__()
+
+class Stack:
+	def __init__(self):
+		self.stack = []
+
+	def put(self, elem):
+		self.stack.append(elem)
+
+	def get(self):
+		return self.stack.pop()
+
+	def __len__(self):
+		return len(self.stack)
+
+	def __str__(self):
+		return self.stack.__str__()
+
+class Graph:
+	def __init__(self, adj):
+		self.adj = adj
+
+	def bfs(self, s):
+		visited = set()
+		queue = Queue()
+		queue.put(s)
+
+		while len(queue) > 0:
+			e = queue.get()
+
+			if e in visited:
+				continue
+
+			visited.add(e)
+
+			print (e + " ", end = '')
+
+			for v in self.adj[e]:
+				if v in visited:
+					continue
+
+				queue.put(v)
+
+		print ("")
